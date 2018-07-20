189e24e3c1b9bd1ef09c95edee10e1e1ac70cd33
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-20 14:11:30 +0000

    automatically backed up by learn

--- db/migrate/20180720140421_create_appointments.rb
+++ db/migrate/20180720140421_create_appointments.rb
@@ -1,8 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.date :date
-      t.time :time
+      t.datetime :datetime
       t.integer :patient_id
       t.integer :doctor_id
 
