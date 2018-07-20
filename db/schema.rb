53f721f408106ebc2f8ef1f5582406d28d411a93
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-20 14:13:45 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180720140421) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.datetime "datetime"
-    t.integer  "patient_id"
-    t.integer  "doctor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
